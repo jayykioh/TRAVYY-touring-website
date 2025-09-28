dfce672983a1ca8fcd25169027b4eb86b0b79416
diemkieu888 <tranthidiemkieu2k5@gmail.com>
2025-09-28 12:52:51 +0700

    WIP: dang code o App.jsx, mainhome.Jsx

--- touring-fe/src/App.jsx
+++ touring-fe/src/App.jsx
@@ -1,26 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
-
-
 import Home from "./pages/Home";
-import MainHome from  "./pages/MainHome";
+import MainHome from "./pages/MainHome";
 import DestinationPage from "./pages/Blogs";
 import SearchResults from "./pages/SearchResults";
-
-
+import DiscountCodesPage from "./pages/DiscountCodesPage"; // Import trang mã giảm giá
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <Routes >
+    <Routes>
       <Route element={<MainLayout/>}>
         <Route path="/" element={<Home />} />
-      <Route path="/destinations/:slug" element={<DestinationPage />} />
-      <Route path="/search" element={<SearchResults />} />
-       <Route path="/home" element={< MainHome/>} />
-      <Route path="*" element={<div className="p-6">404</div>} />
-     
+        <Route path="/destinations/:slug" element={<DestinationPage />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/home" element={<MainHome/>} />
+        <Route path="/discount-codes" element={<DiscountCodesPage />} /> {/* Route mới */}
+        <Route path="*" element={<div className="p-6">404</div>} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
