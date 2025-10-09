b4399ad221496dafba5a2f62a82ceb6c642b79b1
Adnparr <adnparr@gmail.com>
2025-10-09 13:54:58 +0700

    update ảnh nha khe p1

--- touring-fe/src/utils/imageUrl.js
+++ touring-fe/src/utils/imageUrl.js
@@ -6,9 +6,5 @@ export const optimizeImage = (url, width = 1920) => {
       `/upload/f_auto,q_auto,dpr_auto,w_${width},c_scale/`
     );
   }
-  return `https://cdn.statically.io/img/${url.replace(
-    /^https?:\/\//,
-    ""
-  )}?quality=80&width=${width}`;
 };
 // Thêm các tham số tối ưu hóa ảnh cho các dịch vụ phổ biến
